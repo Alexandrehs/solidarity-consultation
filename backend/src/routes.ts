fcb59d6350230d7e1eec72191008953b19bde4af
Alexandrehs <echo.alexandre@gmail.com>
2020-07-23 09:32:03 -0300

    implementaçao das rotas requests no routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,19 @@
 import {Router} from 'express';
 
 import UsersController from './controllers/UsersController';
+import RequestsController from './controllers/RequestsController';
 
 const routes = Router();
 
 const usersController = new UsersController();
+const requestsController = new RequestsController();
 
 routes.get('/users', usersController.index);
+routes.get('/users/:id', usersController.show);
+routes.post('/users', usersController.create);
+
+routes.get('/requests', requestsController.index);
+routes.get('/requests/:id', requestsController.show);
+routes.post('/requests', requestsController.create);
 
 export default routes;
