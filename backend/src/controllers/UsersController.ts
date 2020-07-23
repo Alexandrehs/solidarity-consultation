8474ca630156a0536eb95d30b04473dbe86b2c33
Alexandrehs <echo.alexandre@gmail.com>
2020-07-23 09:09:49 -0300

    correçao no package.json alteraçåo de ts-node para ts-node-dev nos scripts

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -5,6 +5,14 @@ class UsersController {
   async index(request: Request, response: Response) {
     return response.json({mss: 'Todos os abestadus listados aqui!'});
   }
+
+  async create(request: Request, response: Response) {
+    const requestDataNewUser = request.body;
+
+    const idNewUser = await knex('users').insert(requestDataNewUser);
+
+    return response.json({id: idNewUser, requestDataNewUser});
+  }
 }
 
 export default UsersController;
